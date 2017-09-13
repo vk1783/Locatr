4dc3694af1a69a41facf5662a469639a95a3f5c7
Vladislav <mr.vk96@gmail.com>
2017-09-13 20:51:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/locatr/LocatrFragment.java
@@ -0,0 +1,45 @@
+package com.example.locatr;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Влад on 05.09.2017.
+ */
+
+public class LocatrFragment extends Fragment {
+
+    private ImageView mImageView;
+
+    public static LocatrFragment newInstance()
+    {
+        return new LocatrFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
+        mImageView = (ImageView) v.findViewById(R.id.image);
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
+    }
+}
